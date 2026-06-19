63a71cc0244481fdd8f483bc52e4c2053343592b
Nicolas <nicolas.antauf@gmail.com>
2026-06-19 20:43:57 +0200

    add: correct or incorrect answer evaluation

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,6 +8,9 @@
 
 #define MIN 'A'
 #define MAX 'Z'
+
+#define CORRECT_STRING "Correct!\n"
+#define INCORRECT_STRING "Wrong...\n"
 
 struct letter_pair {
     char first;
@@ -32,26 +36,56 @@ struct letter_pair get_letter_pair() {
     return result;
 }
 
-void print_instructions() { puts("\n"); }
-
 void print_question(struct letter_pair pair) {
-    printf("Which letter comes first in the alphabet: %c or %c\n? ", pair.first,
+    printf("Which letter comes first in the alphabet: %c or %c?\n", pair.first,
            pair.second);
 }
 
 void print_invalid_answer(void) {
-    printf("Please answer with a single letter\n");
+    printf("Please answer with either of the letters above.\n");
 }
 
-int get_result(struct letter_pair pair, char *answer) {
+enum MATCH_LEVEL { NONE, FIRST, SECOND };
+
+enum MATCH_LEVEL get_match(struct letter_pair pair, char sanitized_answer) {
+    if (sanitized_answer == pair.first) {
+        return FIRST;
+    } else if (sanitized_answer == pair.second) {
+        return SECOND;
+    } else {
+        return NONE;
+    }
+}
+
+enum ANSWER_VALIDITY { ANSWER_INVALID, ANSWER_INCORRECT, ANSWER_CORRECT };
+
+enum ANSWER_VALIDITY get_result(struct letter_pair pair, char *answer) {
     size_t real_answer_len = strlen(answer);
+    enum MATCH_LEVEL match = NONE;
 
     if (real_answer_len != 2) {
         print_invalid_answer();
-        return -1;
+        return ANSWER_INVALID;
+    }
+    match = get_match(pair, (char)toupper(answer[0]));
+
+    if (match == NONE) {
+        print_invalid_answer();
+        return ANSWER_INVALID;
     }
 
-    return 0;
+    if (match == FIRST && pair.first <= pair.second) {
+        puts(CORRECT_STRING);
+        return ANSWER_CORRECT;
+    }
+
+    if (match == SECOND && pair.second <= pair.first) {
+        puts(CORRECT_STRING);
+        return ANSWER_CORRECT;
+    }
+
+    puts(INCORRECT_STRING);
+    return ANSWER_INCORRECT;
 }
 
 void game_loop() {
@@ -59,18 +93,22 @@ void game_loop() {
     char *answer = NULL;
     size_t answer_len = 0;
     ssize_t answer_read = 0;
+    enum ANSWER_VALIDITY validity = ANSWER_INVALID;
 
     for (;;) {
         pair = get_letter_pair();
         print_question(pair);
-        answer_read = getline(&answer, &answer_len, stdin);
 
-        if (answer_read == -1 || strncmp(answer, "quit", 4) == 0) {
-            free(answer);
-            return;
+        validity = ANSWER_INVALID;
+        while (validity == ANSWER_INVALID) {
+            answer_read = getline(&answer, &answer_len, stdin);
+            if (answer_read == -1 || strncmp(answer, "quit", 4) == 0) {
+                free(answer);
+                return;
+            }
+
+            validity = get_result(pair, answer);
         }
-
-        get_result(pair, answer);
     }
 }
 
