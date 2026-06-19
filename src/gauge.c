f9521e39460f5e86df7a6c193fbaee045b7cecaf
Nicolas <nicolas.antauf@gmail.com>
2026-06-20 01:19:00 +0200

    remove: dumb mistake

--- src/gauge.c
+++ src/gauge.c
@@ -1,27 +1,16 @@
 #include "common.h"
-#include <stdint.h>
 #include <stdio.h>
 
 void print_gauge(struct letter_pair pair) {
     putchar(' ');
     for (char i = MIN_CHAR; i <= MAX_CHAR; i += 1) {
-        if (i == pair.first) {
-            putchar(pair.first);
-        } else if (i == pair.second) {
-            putchar(pair.second);
-        } else {
-            putchar(' ');
-        }
+        putchar((i == pair.first || i == pair.second) ? i : ' ');
     }
     putchar('\n');
 
     putchar(' ');
     for (char i = MIN_CHAR; i <= MAX_CHAR; i += 1) {
-        if (i == pair.first || i == pair.second) {
-            putchar('|');
-        } else {
-            putchar(' ');
-        }
+        putchar((i == pair.first || i == pair.second) ? '|' : ' ');
     }
     putchar('\n');
 
